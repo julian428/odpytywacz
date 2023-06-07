0422b3204c73c87bae9f2c5c30c9beffa889c218
Julian Metzger <julianjometzger@gmail.com>
2023-06-07 10:13:06 +0200

    updated the quiz loading skeleton

--- src/app/quizes/[quizId]/loading.tsx
+++ src/app/quizes/[quizId]/loading.tsx
@@ -1,51 +1,46 @@
-import Button from "@/components/ui/button";
 import Container from "@/components/ui/container";
-import H1 from "@/components/ui/headings/h1";
-import H2 from "@/components/ui/headings/h2";
-import Input from "@/components/ui/inputs/input";
-import React from "react";
 
 export default function loading() {
   return (
     <>
-      <section className="text-center mt-16 space-y-2 animate-pulse">
-        <H1>tytuł</H1>
-        <section className="flex gap-2 justify-center items-center">
-          <p className="opacity-80">0%</p>
-          <progress
-            className="progress progress-success w-56"
-            value={0}
-            max={100}
+      <article className="mt-8 flex flex-col items-center gap-8 px-4">
+        <header className="h-16 w-96 bg-white opacity-80 animate-pulse rounded-2xl" />
+        <nav>
+          <div className="btn-group">
+            <div className="btn btn-disabled">«</div>
+            <div className="btn">pytanie 1</div>
+            <div className="btn animate-pulse">»</div>
+          </div>
+        </nav>
+        <Container className="p-8 flex flex-col items-center gap-8 lg:w-2/5 w-full">
+          <div className="h-12 w-32 bgwhite opacity-80 animate-pulse rounded-2xl" />
+          <div className="badge badge-error w-24 animate-pulse" />
+          <input
+            type="text"
+            placeholder="odpowiedź..."
+            disabled
+            className="input text-2xl input-lg input-bordered w-full max-w-md border-2 animate-pulse"
           />
-          <p className="opacity-80">100%</p>
-        </section>
-      </section>
-      <article className="flex lg:w-fit w-full justify-center gap-4 relative mx-auto lg:mt-16 mt-8">
-        <section className="w-full">
-          <Container className="lg:w-[800px] lg:h-[450px] p-4 w-full flex flex-col lg:bg-opacity-50 bg-opacity-0 justify-center items-center lg:gap-8 gap-4 relative animate-pulse">
-            <H2>pytanie</H2>
-            <Input className="lg:text-5xl lg:p-4 lg:w-[400px] w-4/5 border-2 border-transparent" />
-
-            <footer className="absolute flex lg:w-full gap-4 lg:scale-100 scale-75 w-fit justify-between px-2 lg:bottom-2 -bottom-8">
-              <Button
-                variant="ghost"
-                bg="dark"
+          <div className="divider" />
+          <div className="divider" />
+          <div className="badge animate-pulse w-24" />
+          <footer className="w-full px-8 flex justify-between items-center">
+            <button className="btn btn-sm btn-disabled">zakończ</button>
+            <section className="flex gap-4 items-center">
+              <div
+                className="tooltip tooltip-primary"
+                data-tip="pomoc"
               >
-                zakończ
-              </Button>
-              <section className="flex gap-4">
-                <Button
-                  variant="ghost"
-                  bg="dark"
-                  className="w-9 h-9 rounded-full flex justify-center items-center text-[24px]"
-                >
-                  <p>?</p>
-                </Button>
-                <Button bg="light">sprawdź</Button>
-              </section>
-            </footer>
-          </Container>
-        </section>
+                <button className="btn btn-sm text-xl btn-disabled btn-outline btn-circle">
+                  ?
+                </button>
+              </div>
+              <button className="btn btn-sm btn-primary btn-disabled">
+                sprawdź
+              </button>
+            </section>
+          </footer>
+        </Container>
       </article>
     </>
   );
