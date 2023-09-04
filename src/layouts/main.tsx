8e881235d03ea1865f02859315a4a5575564d018
julian428 <julianjometzger@gmail.com>
2023-09-04 12:21:16 +0200

    added google adsense and updated all packages

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,27 +1,35 @@
 import Profile from "@/components/layout/profile";
 import Link from "next/link";
 import { Suspense } from "react";
+import Script from "next/script";
 
 export default function MainNav() {
   return (
-    <nav className="flex lg:justify-around justify-between py-4 px-1 items-center">
-      <Link href="/">
-        <h1 className="font-black text-2xl">ODPYTYWACZ</h1>
-      </Link>
-      <section className="flex lg:gap-4 gap-2 items-center">
-        <Link href="/quizes">quizy</Link>
-        <Link href="/blogs">blogi</Link>
-        <Suspense
-          fallback={
-            <div className="btn btn-circle btn-sm">
-              <div className="rounded-full avatar animate-pulse" />
-            </div>
-          }
-        >
-          {/* @ts-expect-error Async Server Component*/}
-          <Profile />
-        </Suspense>
-      </section>
-    </nav>
+    <>
+      <Script
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-xxxxxxxxxxxxxxx"
+      />
+      <nav className="flex lg:justify-around justify-between py-4 px-1 items-center">
+        <Link href="/">
+          <h1 className="font-black text-2xl">ODPYTYWACZ</h1>
+        </Link>
+        <section className="flex lg:gap-4 gap-2 items-center">
+          <Link href="/quizes">quizy</Link>
+          <Link href="/blogs">blogi</Link>
+          <Suspense
+            fallback={
+              <div className="btn btn-circle btn-sm">
+                <div className="rounded-full avatar animate-pulse" />
+              </div>
+            }
+          >
+            {/* @ts-expect-error Async Server Component*/}
+            <Profile />
+          </Suspense>
+        </section>
+      </nav>
+    </>
   );
 }
