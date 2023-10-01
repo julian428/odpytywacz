974cec374956b71c00b3d43093a8b54fc043b379
Julian <julianjometzger@gmail.com>
2023-10-01 17:18:29 +0200

    starting from scratch

--- src/components/ui/headings/h1.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function H1({ children }: Props) {
-  return <h2 className="lg:text-6xl text-3xl font-black">{children}</h2>;
-}
--- src/components/ui/headings/h4.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function H4({ children }: Props) {
-  return <h5 className="text-lg truncate">{children}</h5>;
-}
