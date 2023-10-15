8a7afd979fde4a68978229a93c185ebdfaa6ded5
Julian <julianjometzger@gmail.com>
2023-10-15 09:52:29 +0200

    The main page is somewhat functional. It displays all the available items are displayed and link to the corresponding page.

---
+++ src/lib/icons.tsx
@@ -0,0 +1,5 @@
+export { MdOutlineDashboardCustomize as DashboardIcon } from "react-icons/md";
+export { CgProfile as ProfileIcon } from "react-icons/cg";
+export { ImStatsBars2 as StatsIcon } from "react-icons/im";
+export { AiOutlineSearch as SearchIcon } from "react-icons/ai";
+export { BiFilterAlt as FilterIcon } from "react-icons/bi";
