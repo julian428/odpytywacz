fd1d3d4acc5370b2e38239288a9ecc3f5eea097b
Julian <julianjometzger@gmail.com>
2023-10-18 20:05:01 +0200

    finished the basic workings of the file tree.

--- src/pages/api/questions.json.ts
+++ src/pages/api/questions.json.ts
@@ -3,16 +3,12 @@ import PocketBase from 'pocketbase'
 const dbAddress = import.meta.env.PB_LOCATION;
 const pb = new PocketBase(dbAddress)
 
-export async function GET({request}: {request: Request}){
+export async function GET({request}:{request: Request}) {
     let query = new URL(request.url).searchParams.get("query") as string
     query = query.replaceAll('_and_', '&&')
-    const list = await pb.collection('item').getFullList({
+    console.log('trigerred')
+    const list = await pb.collection('question').getFullList({
         filter: query
     })
     return new Response(JSON.stringify(list))
-}
-
-export async function POST({request}: {request: Request}){
-    const body = await request.json()
-    return new Response(JSON.stringify(body))
 }
