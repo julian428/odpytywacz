64c948823e6528931ddd1bb9799cd95c4065f81e
Julian <julianjometzger@gmail.com>
2023-10-19 17:30:55 +0200

    fixed a api bug

--- src/pages/api/items.json.ts
+++ src/pages/api/items.json.ts
@@ -4,7 +4,10 @@ const dbAddress = import.meta.env.PB_LOCATION;
 const pb = new PocketBase(dbAddress)
 
 export async function GET({request}: {request: Request}){
-    let query = new URL(request.url).searchParams.get("query") as string
+    let query = new URL(request.url).searchParams.get("query")
+    if(typeof query != "string"){
+        query = ""
+    }
     query = query.replaceAll('_and_', '&&')
     const list = await pb.collection('item').getFullList({
         filter: query
