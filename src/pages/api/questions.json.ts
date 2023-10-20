da47cc3884b272a259cc5fd6e7deedafef03e4cb
Julian <julianjometzger@gmail.com>
2023-10-20 16:55:31 +0200

    added a template reader to the question endpoint

--- src/pages/api/questions.json.ts
+++ src/pages/api/questions.json.ts
@@ -6,9 +6,9 @@ const pb = new PocketBase(dbAddress)
 export async function GET({request}:{request: Request}) {
     let query = new URL(request.url).searchParams.get("query") as string
     query = query.replaceAll('_and_', '&&')
-    console.log('trigerred')
     const list = await pb.collection('question').getFullList({
         filter: query
     })
-    return new Response(JSON.stringify(list))
+    const html = list.map((question) => `<li><a href=${question.id+'/question'}>${question.answears}</a></li>`).join().replaceAll(',', '')
+    return new Response(html)
 }
