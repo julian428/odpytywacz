6e2d76b77c6fa7d50a9b90a09c3740d9eed76159
Julian <julianjometzger@gmail.com>
2023-10-21 19:36:02 +0200

    now you can save the article content

--- src/pages/api/items.json.ts
+++ src/pages/api/items.json.ts
@@ -14,3 +14,11 @@ export async function GET({request}:{request: Request}) {
     })
     return new Response(JSON.stringify(list))
 }
+
+export async function PUT({request}:{request: Request}) {
+    const data = await request.json()
+
+    console.log(data)
+    await pb.collection('item').update(data.id, {content: data.content})
+    return new Response("saved")
+}
