c8c23150048014219dc72a201218ee4b7400f522
Julian <julianjometzger@gmail.com>
2023-10-25 17:11:39 +0200

    Now the file tree can render records

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,62 @@
 /// <reference types="astro/client" />
 declare namespace App {
     interface Locals {
-        user: any
+        user: {
+            record: {
+                collectionId: string,
+                collectionName: 'users',
+                created: string,
+                email: string,
+                emailVisibility: boolean,
+                id: string,
+                updated: string,
+                username: string,
+                verified: boolean
+              },
+              token: string
+        } | null
         loggedIn: boolean
     }
+
+}
+interface Playlist {
+    id: string
+    collectionId: string
+    collectionName: "playlist"
+    created: string
+    updated: string
+    title: string
+    description: string
+    owner: string
+}
+
+interface Item {
+    id: string
+    created: string
+    updated: string
+    collectionId: string
+    collectionName: "item"
+    playlist: string
+    owner: string
+    title: string
+    description: string
+    content: string
+    color: string
+    cover: string
+}
+
+interface Question {
+    id: string
+    collectionId: string
+    collectionName: string
+    created: string
+    updated: string
+    type: "open" | "close"
+    question: string
+    answears: string
+    decoys: string
+    item: string
+    owner: string
+    title: string
+    show: "2:1"|"2:2"|"3:1"|"3:2"|"3:3"|"4:1"|"4:2"|"4:3"|"4:4"
 }
